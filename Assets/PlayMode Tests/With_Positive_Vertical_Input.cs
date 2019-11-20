a2ac0546b4c8a2820429bce4cd8608901ca700e0
mharris161 <mharris161@gmail.com>
2019-11-19 22:10:26 -0500

    Added Rotator script to the player to handle rotations based on the Mouse X position. Created a new test to simply test that the player has rotated over time given a negative Mouse X input.

--- Assets/PlayMode Tests/With_Positive_Vertical_Input.cs
+++ Assets/PlayMode Tests/With_Positive_Vertical_Input.cs
@@ -3,38 +3,80 @@ using System.Collections;
 using NSubstitute;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.TestTools;
 
 namespace A_Player
 {
-    public class With_Positive_Vertical_Input
+    public static class Helpers
     {
-        [UnityTest]
-        public IEnumerator Moves_Forward()
+        public static void CreateFloor()
         {
             var floor = GameObject.CreatePrimitive(PrimitiveType.Cube);
             floor.transform.localScale = new Vector3(50, 0.1f, 50);
             floor.transform.position = Vector3.zero;
+        }
 
+        public static Player CreatePlayer()
+        {
             var playerGameObject = GameObject.CreatePrimitive(PrimitiveType.Capsule);
             playerGameObject.AddComponent<CharacterController>();
+            playerGameObject.AddComponent<NavMeshAgent>();
             playerGameObject.transform.position = new Vector3(0, 1.3f, 0);
             
             Player player = playerGameObject.AddComponent<Player>();
             
             var testPlayerInput = Substitute.For<IPlayerInput>();
             player.PlayerInput = testPlayerInput;
-            
-            testPlayerInput.Vertical.Returns(1f);
-            testPlayerInput.Horizontal.Returns(4f);
+            return player;
+        }
 
-            float startingZPosition = playerGameObject.transform.position.z;
+        public static float CalculateTurn(Quaternion originalRotation, Quaternion newRotation)
+        {
+            var cross = Vector3.Cross(originalRotation * Vector3.forward, newRotation * Vector3.forward);
+            var dot = Vector3.Dot(cross, Vector3.up);
+            return dot;
+        }
+    }
+    
+    public class With_Positive_Vertical_Input
+    {
+        [UnityTest]
+        public IEnumerator Moves_Forward()
+        {
+            Helpers.CreateFloor();
+
+            var player = Helpers.CreatePlayer();
+            
+            player.PlayerInput.Vertical.Returns(1f);
+            player.PlayerInput.Horizontal.Returns(4f);
+
+            float startingZPosition = player.transform.position.z;
             
             yield return new WaitForSeconds(5f);
             
             float endingZPosition = player.transform.position.z;
 
             Assert.Greater(endingZPosition, startingZPosition);
+        }
+    }
+
+    public class With_Negative_Mouse_X
+    {
+        [UnityTest]
+        public IEnumerator Turns_Left()
+        {
+            Helpers.CreateFloor();
+
+            var player = Helpers.CreatePlayer();
+            
+            player.PlayerInput.MouseX.Returns(-1f);
+
+            var originalRotation = player.transform.rotation;
+            yield return new WaitForSeconds(0.5f);
+
+            float turnAmount = Helpers.CalculateTurn(originalRotation, player.transform.rotation);
+            Assert.Less(turnAmount, 0);
         }
     }
 }
