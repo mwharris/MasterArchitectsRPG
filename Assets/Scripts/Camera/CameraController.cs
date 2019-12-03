d2364e4205ed7f849ca3f597cae72305ac1d9792
mharris161 <mharris161@gmail.com>
2019-12-02 22:46:49 -0500

    Created a CameraController script for looking up and down with the camera.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float _tilt;
+
+    void Update()
+    {
+        float mousePosition = Input.GetAxis("Mouse Y");
+        _tilt = Mathf.Clamp(_tilt - mousePosition, -15f, 15f);
+        transform.localRotation = Quaternion.Euler(_tilt, 0, 0);
+    }
+}
