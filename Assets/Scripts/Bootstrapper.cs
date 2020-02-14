a9b9ea39962ed882712d2a73041862cb0623e100
mharris161 <mharris161@gmail.com>
2020-02-14 06:47:54 -0500

    Created a Bootstrapper class with a function using [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]. This should run that function before the scene starts loading in order to initialize PlayerInput before anything else.

---
+++ Assets/Scripts/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bootstrapper
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    public static void Initialize()
+    {
+        GameObject inputGameObject = new GameObject("[INPUT SYSTEM]");
+        inputGameObject.AddComponent<PlayerInput>();
+        GameObject.DontDestroyOnLoad(inputGameObject);
+    }
+}
