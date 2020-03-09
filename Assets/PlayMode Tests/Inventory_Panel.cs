ac30254ff0952ee41f15b3e2cca19f27b28f7886
mharris161 <mharris161@gmail.com>
2020-03-09 07:52:00 -0400

    Created and worked on a new Play Mode test for our inventory slot rewrite. Followed TDD to go back and forth between working on the test and production code. Created UIInventoryPanel, InventoryPanel prefab, InventorySlot prefab.

---
+++ Assets/PlayMode Tests/Inventory_Panel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace PlayMode_Tests
+{
+    public class Inventory_Panel
+    {
+        private UIInventoryPanel GetInventoryPanel()
+        {
+            var prefab = AssetDatabase.LoadAssetAtPath<UIInventoryPanel>("Assets/Prefabs/UI/InventoryPanel.prefab");
+            return Object.Instantiate(prefab);
+        }
+        
+        [Test]
+        public void Has_25_Slots()
+        {
+            var inventoryPanel = GetInventoryPanel();
+            Assert.AreEqual(25, inventoryPanel.SlotCount);
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/UIInventoryPanel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class UIInventoryPanel : MonoBehaviour
+{
+    private UIInventorySlot[] _slots;
+    public int SlotCount => _slots.Length;
+
+    private void Awake()
+    {
+        _slots = GetComponentsInChildren<UIInventorySlot>();
+    }
+}
